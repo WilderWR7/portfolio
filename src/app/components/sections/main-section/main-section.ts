5dd158dd0bd7b19f291db4d5e3f2c5eba445f5ef
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 14:14:18 -0400

    feat: add main section component with HTML template and TypeScript class

---
+++ src/app/components/sections/main-section/main-section.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeroSection } from "../hero-section/hero-section";
+import { AboutMeSection } from "../about-me-section/about-me-section";
+import { SummarySection } from "../summary-section/summary-section";
+import { SkillsSection } from "../skills-section/skills-section";
+import { ProjectSection } from "../project-section/project-section";
+import { ExperienceSection } from "../experience-section/experience-section";
+import { EducationSection } from "../education-section/education-section";
+
+@Component({
+  selector: 'app-main-section',
+  imports: [HeroSection, AboutMeSection, SummarySection, SkillsSection, ProjectSection, ExperienceSection, EducationSection],
+  templateUrl: './main-section.html',
+})
+export class MainSection { }
