ab5e2043aa2adba012967fa53b235f42701b9be7
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 17:37:08 -0400

    feat: add main section to the app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
+import { MainSection } from "./components/sections/main-section/main-section";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, MainSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('portafolio-1');
-}
+export class App {}
