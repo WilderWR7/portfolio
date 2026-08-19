17d3b97e5c2d65512d090dc2b7a594767d8edc48
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 14:02:23 -0400

    feat: add summary section component with HTML template

---
+++ src/app/components/sections/summary-section/summary-section.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'summary-section',
+  imports: [],
+  templateUrl: './summary-section.html',
+})
+export class SummarySection {}
