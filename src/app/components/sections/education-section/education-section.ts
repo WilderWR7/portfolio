cdab166058309e18b59156feebd6cd5bc266e503
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 14:08:37 -0400

    feat: add education section component with HTML template and TypeScript class

---
+++ src/app/components/sections/education-section/education-section.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'education-section',
+  imports: [],
+  templateUrl: './education-section.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EducationSection {}
