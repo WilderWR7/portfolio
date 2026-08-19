8acecd58b6424f1e179c2c8101946142768c1015
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 14:07:31 -0400

    feat: add contact section component with HTML template and TypeScript class

---
+++ src/app/components/sections/contact-section/contact-section.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-section',
+  imports: [],
+  templateUrl: './contact-section.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactSection {}
