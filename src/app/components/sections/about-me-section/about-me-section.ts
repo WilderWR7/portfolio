f6f184b968e7023291af9a6d93a0ea8af0e3d7df
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 14:03:10 -0400

    feat: add about me section component with HTML template and TypeScript class

---
+++ src/app/components/sections/about-me-section/about-me-section.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-me-section',
+  imports: [],
+  templateUrl: './about-me-section.html',
+})
+export class AboutMeSection {}
