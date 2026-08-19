9a8347940b3b20a3b93f04e49204b3a745ae14b3
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 14:12:31 -0400

    feat: add project section component with HTML template and TypeScript class

---
+++ src/app/components/sections/project-section/project-section.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-section',
+  imports: [],
+  templateUrl: './project-section.html',
+})
+export class ProjectSection {}
