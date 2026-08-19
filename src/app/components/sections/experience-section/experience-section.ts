7660ae1690c4c770b77861bf43b2bf16c361ca01
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-19 14:09:36 -0400

    feat: add experience section component with HTML template and TypeScript class

---
+++ src/app/components/sections/experience-section/experience-section.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'experience-section',
+  imports: [],
+  templateUrl: './experience-section.html',
+})
+export class ExperienceSection { }
