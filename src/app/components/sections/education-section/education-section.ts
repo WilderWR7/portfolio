1a6a99d903ed67ab809857d2d18951faca424982
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-20 11:06:05 -0400

    feat: add education and certifications data to education section

--- src/app/components/sections/education-section/education-section.ts
+++ src/app/components/sections/education-section/education-section.ts
@@ -6,4 +6,38 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './education-section.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EducationSection {}
+export class EducationSection {
+  educations = [
+    {
+      degree: "Licenciado en Informática",
+      institution: "Universidad Mayor de San Andrés (UMSA)",
+      period: "Graduado en 2024"
+    }
+  ];
+  certifications = [
+    {
+      name: "Desarrollo Web Completo con html5, css3, js ajax php y mysql",
+      platform: "Udemy"
+    },
+    {
+      name: "React: De cero a experto ( Hooks y MERN )",
+      platform: "Udemy"
+    },
+    {
+      name: "React Native: Aplicaciones nativas para IOS y Android",
+      platform: "Udemy"
+    },
+    {
+      name: "Next.js: El framework de React para producción",
+      platform: "Udemy"
+    },
+    {
+      name: "Nest: Desarrollo backend escalable con Node",
+      platform: "Udemy"
+    },
+    {
+      name: "Fundamentos del desarrollo web: Full Stack o Front-end",
+      platform: "LinkedIn"
+    }
+  ];
+}
