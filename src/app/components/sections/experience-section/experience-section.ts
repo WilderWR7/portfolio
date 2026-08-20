e004eaa73b06b1ef06390978ef6c25d0d328e7b8
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-20 11:06:21 -0400

    feat: add experience section with dynamic content for roles and descriptions

--- src/app/components/sections/experience-section/experience-section.ts
+++ src/app/components/sections/experience-section/experience-section.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './experience-section.html',
 })
-export class ExperienceSection { }
+export class ExperienceSection {
+  experience = [
+    {
+      period: "Diciembre 2023 - Marzo 2026",
+      role: "Desarrollador Full Stack / Backend",
+      company: "BTSOFT",
+      description: "Asignado a múltiples proyectos de gran escala para clientes internacionales (Open Mundo, Open English Junior y Serena & Lily). Soporte a plataformas de producción, resolución de problemas críticos en producción, desarrollo de nuevas características backend e integración de servicios en la nube (AWS) y bases de datos."
+    }
+  ]
+}
