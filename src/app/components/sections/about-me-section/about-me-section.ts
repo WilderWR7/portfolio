0c9057ddf0f9881183d1a8027eecde03d6696936
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-20 11:04:32 -0400

    feat: implement about section with dynamic content for description and stats

--- src/app/components/sections/about-me-section/about-me-section.ts
+++ src/app/components/sections/about-me-section/about-me-section.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './about-me-section.html',
 })
-export class AboutMeSection {}
+export class AboutMeSection {
+  about = {
+    title: "Sobre Mí",
+    description: "Desarrollador Web Full Stack con más de 2 años de experiencia en el diseño, mantenimiento y optimización de aplicaciones web escalables para clientes internacionales. Utilizando tecnologías como: JavaScript/TypeScript (React, Next.js, NestJS) y PHP (Laravel), con fuerte enfoque en la lógica del lado del servidor (backend) y resolución de problemas complejos.\n\nExperiencia práctica trabajando con arquitecturas basadas en contenedores (Docker) e integrando servicios en la nube (AWS). Busco aportar mis habilidades técnicas en un equipo dinámico, creando soluciones eficientes y estables.",
+    stats: [
+      { label: "Años de Experiencia", value: "2+" },
+      { label: "Proyectos Corporativos", value: "3" },
+      { label: "Educación", value: "UMSA" }
+    ]
+  };
+}
