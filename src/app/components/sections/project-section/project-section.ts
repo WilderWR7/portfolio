c6f3434af565a72f8863e2d827f20b283af32122
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-20 11:08:56 -0400

    feat: implement dynamic project cards in project section

--- src/app/components/sections/project-section/project-section.ts
+++ src/app/components/sections/project-section/project-section.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Project } from '../../../interfaces/project.interface';
 
 @Component({
   selector: 'project-section',
   imports: [],
   templateUrl: './project-section.html',
 })
-export class ProjectSection {}
+export class ProjectSection {
+  projects = input.required<Project[]>();
+}
