acab1fc862308f5aefe069f84af5a7f45dc0124c
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-20 11:08:45 -0400

    feat: add projects input to project section and include contact section in main layout

--- src/app/components/sections/main-section/main-section.ts
+++ src/app/components/sections/main-section/main-section.ts
@@ -6,12 +6,15 @@ import { SkillsSection } from "../skills-section/skills-section";
 import { ProjectSection } from "../project-section/project-section";
 import { ExperienceSection } from "../experience-section/experience-section";
 import { EducationSection } from "../education-section/education-section";
+import { Project } from '../../../interfaces/project.interface';
+import { ContactSection } from "../contact-section/contact-section";
 
 @Component({
   selector: 'app-main-section',
-  imports: [HeroSection, AboutMeSection, SummarySection, SkillsSection, ProjectSection, ExperienceSection, EducationSection],
+  imports: [HeroSection, AboutMeSection, SummarySection, SkillsSection, ProjectSection, ExperienceSection, EducationSection, ContactSection],
   templateUrl: './main-section.html',
 })
 export class MainSection { 
   name = input.required<string>();
+  projects = input.required<Project[]>();
 }
