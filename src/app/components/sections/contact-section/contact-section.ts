4a9b0d2f611872f3579ceed980961a9d9bac592f
WilderWR7 <wilder.mayta91@gmail.com>
2026-08-20 11:05:44 -0400

    feat: update contact section to show dynamic data

--- src/app/components/sections/contact-section/contact-section.ts
+++ src/app/components/sections/contact-section/contact-section.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'contact-section',
   imports: [],
   templateUrl: './contact-section.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactSection {}
+export class ContactSection {
+  contact = {
+    email: "wilder.mayta91@gmail.com",
+    location: "La Paz - Bolivia",
+    availability: "Disponible para integrarse a equipos dinámicos y proyectos desafiantes."
+  }
+}
